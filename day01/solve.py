76e0576989fe2962b517621febe4370faabedfbd
waylow1 <max2742contact@gmail.com>
2024-12-02 18:05:50 +0100

    first days + ReadME

--- day01/solve.py
+++ day01/solve.py
@@ -1,12 +1,18 @@
-def solve_1(data):
-    pass
+def solve_1(first_list, second_list):
+    return sum(abs(a - b) for a, b in zip(sorted(map(int, first_list)), sorted(map(int, second_list))))
 
-def solve_2(data):
-    pass
+def solve_2(first_list, second_list):
+    nb_occurences_b = {int(b): second_list.count(b) for b in set(second_list)}
+    return sum(int(a) * nb_occurences_b.get(int(a), 0) for a in first_list)
+
+    
+
+def main():
+    with open("input.txt") as f:
+        data = [line.strip().split(" ") for line in f]
+    first_list,second_list= [int(x[0]) for x in data], [int(x[-1]) for x in data]
+    print(solve_1(first_list, second_list))
+    print(solve_2(first_list, second_list))
 
 if __name__ == "__main__":
-    with open("input.txt", "r") as f:
-        data = f.read().strip()
-    
-    print("Part 1:", solve_1(data))
-    print("Part 2:", solve_2(data))
+    main()
--- day02/solve.py
+++ day02/solve.py
@@ -1,12 +1,20 @@
 def solve_1(data):
-    pass
+    count = 0
+    for values in data:
+        ascending_safe = all(i < j and 1 <= (j - i) <= 3 for i, j in zip(values, values[1:]))
+        descending_safe = all(i > j and 1 <= (i - j) <= 3 for i, j in zip(values, values[1:]))
+        if ascending_safe or descending_safe:
+            count += 1
+    return count
+
 
 def solve_2(data):
     pass
 
+def main():
+    with open("input.txt") as f:
+        data = [list(map(int, line.strip().split())) for line in f]
+    print(solve_1(data))
+
 if __name__ == "__main__":
-    with open("input.txt", "r") as f:
-        data = f.read().strip()
-    
-    print("Part 1:", solve_1(data))
-    print("Part 2:", solve_2(data))
+    main()
