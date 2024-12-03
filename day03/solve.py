1ed63d4496fb78618d4d8d07359911845437404b
waylow1 <max2742contact@gmail.com>
2024-12-03 12:55:34 +0100

    day3

--- day03/solve.py
+++ day03/solve.py
@@ -1,8 +1,10 @@
+import re
+
 def solve_1(data):
-    pass
+    return sum(int(r.group(1)) * int(r.group(2)) for r in re.finditer("mul\((\d{1,3}),(\d{1,3})\)", data))
 
 def solve_2(data):
-    pass
+    return sum(int(r.group(1)) * int(r.group(2)) for active, chunk in zip([True] + [cmd == "do" for cmd in re.findall("(do|don't)\(\)", data)], re.split("do\(\)|don't\(\)", data)) if active for r in re.finditer("mul\((\d{1,3}),(\d{1,3})\)", chunk))
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
